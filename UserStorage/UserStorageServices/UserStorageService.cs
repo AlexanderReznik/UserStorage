b78b539383b45fd7493ef8e953cbf1434737c049
Alexander Reznik <Alexandr_Reznik@epam.com>
2017-10-29 13:23:39 +0300

    Extract interface for UserStorageService added.

--- UserStorage/UserStorageServices/UserStorageService.cs
+++ UserStorage/UserStorageServices/UserStorageService.cs
@@ -6,7 +6,7 @@ namespace UserStorageServices
     /// <summary>
     /// Represents a service that stores a set of <see cref="User"/>s and allows to search through them.
     /// </summary>
-    public class UserStorageService
+    public class UserStorageService : IUserStorageService
     {
         /// <summary>
         /// Public c-tor to initialize storage.
