075588e133079519c4b20812ed88251569bb7ef8
Alexander Reznik <Alexandr_Reznik@epam.com>
2017-11-10 00:26:17 +0300

    Sending xml string in notifications.

--- UserStorage/UserStorageServices/Notifications/NotificationReceiver.cs
+++ UserStorage/UserStorageServices/Notifications/NotificationReceiver.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace UserStorageServices.Notifications
 {
@@ -11,13 +13,22 @@ namespace UserStorageServices.Notifications
     {
         public event Action<NotificationContainer> Received;
 
+        private XmlSerializer Serializer { get; }
+
         public NotificationReceiver()
         {
             Received = container => { };
+            Serializer = new XmlSerializer(typeof(NotificationContainer));
         }
 
-        public void Receive(NotificationContainer container)
+        public void Receive(string message)
         {
+            NotificationContainer container;
+            using (var stringReader = new StringReader(message))
+            {
+                container = (NotificationContainer)Serializer.Deserialize(stringReader);
+            }
+
             Received(container);
         }
     }
--- UserStorage/UserStorageServices/Notifications/NotificationSender.cs
+++ UserStorage/UserStorageServices/Notifications/NotificationSender.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace UserStorageServices.Notifications
 {
@@ -10,14 +12,24 @@ namespace UserStorageServices.Notifications
     {
         public INotificationReceiver Receiver {get; set; }
 
+        private XmlSerializer Serializer { get; }
+
         public NotificationSender(INotificationReceiver receiver = null)
         {
             Receiver = receiver ?? new NotificationReceiver();
+            Serializer = new XmlSerializer(typeof(NotificationContainer));
         }
 
         public void Send(NotificationContainer container)
         {
-            Receiver.Receive(container);
+            string message;
+            using (var stringWriter = new StringWriter())
+            {
+                Serializer.Serialize(stringWriter ,container);
+                message = stringWriter.ToString();
+            }
+            
+            Receiver.Receive(message);
         }
     }
 }
